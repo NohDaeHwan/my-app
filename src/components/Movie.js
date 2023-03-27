7da3f8a0bb4a07b2228a683245c99ebc9cfa1f02
BackendDeveloper <ndh5953@naver.com>
2023-03-27 14:38:58 +0900

    CSS 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Movie.module.css';
 
-function Movie({ id, mediumCoverImage, title, summary, genres }) {
+function Movie({ id, mediumCoverImage, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={mediumCoverImage} alt={title} />
-      <h2>
+    <div className={styles.movie}>
+      <img src={mediumCoverImage} alt={title} className={styles.movie__img} />
+      <h2 className={styles.movie__title}>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
-      <ul>
+      <h3 className={styles.movie__year}>{year}</h3>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+      <ul className={styles.movie__genres}>
         {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -14,8 +15,8 @@ function Home() {
     getMovies();
   }, []);
   return (
-    <div>
-      {loading ? <h1>Loading...</h1> : <div>{movies.map((movie) => 
+    <div className={styles.container}>
+      {loading ? <div className={styles.loader}><span>Loading...</span></div> : <div className={styles.movies}>{movies.map((movie) => 
           <Movie key={movie.id} id={movie.id} mediumCoverImage={movie.medium_cover_image} 
             title={movie.title} summary={movie.summary} genres={movie.genres} />
       )}</div>}
